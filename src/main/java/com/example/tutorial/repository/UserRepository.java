f31904b807156818595885b52c6246e099d8f0a3
Starwars2020 <45616259+Starwars2020@users.noreply.github.com>
2021-08-19 20:32:01 +0900

    Delete src directory

--- src/main/java/com/example/tutorial/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.tutorial.repository;
-
-import com.example.tutorial.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {}
